204233aadb8f0c560ce0fcf05379f05f4d844fbf
Vaz-Tiago <tvaz@redventures.com>
2022-05-14 22:53:41 -0300

    add open and close events

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -7,12 +7,18 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class SideDrawer {
   @Prop({ reflect: true }) titleMenu: string;
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  onCloseDrawer() {
+    this.open = false;
+  }
 
   render() {
     return (
       <aside>
         <header>
           <h1>{this.titleMenu}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
         </header>
         <main>
           <slot />
